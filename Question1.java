be27223b8792d576194ae1589eb5823eaab85c26
Shangetha <64067570+Shangetha@users.noreply.github.com>
2020-05-17 17:32:07 +0530

    completed

--- Question1.java
+++ Question1.java
@@ -2,6 +2,11 @@ public class Question1
 {
   public static void main(String[] args)
   {
+   /**
+     * Print "Hello World!"
+     * 
+     * Hint: System.out.println(...)
+     */
     System.out.println("Hello World!");
   }
 }
