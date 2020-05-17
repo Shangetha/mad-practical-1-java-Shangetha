4020afa6bdec2012f62a85c2302dc296d15a8e2b
Shangetha <64067570+Shangetha@users.noreply.github.com>
2020-05-17 17:35:36 +0530

    completed

--- Question5.java
+++ Question5.java
@@ -24,9 +24,31 @@ public class Question5
      *     > 3
      *     2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
+     **/
     Scanner in = new Scanner(System.in);
+        int cnt[] = new int[100];
+		int i;
+        System.out.println("How many integers would you like to input(Max.100?)");
+		int x = in.nextInt();
+        System.out.println("Input the integers:");
+        for (i = 0; i <x; i++){ 
+         int n = in.nextInt();
+            cnt[--n]++;	
+		}
+		 int max = 0;
+        for (int n : cnt){
+            if (max < n){
+                max = n;
+            }
+        }
+         System.out.println("Mode value(s)in ascending order:");
+        for (i = 0; i < cnt.length; i++){
+            if (cnt[i] == max){
+                System.out.println(i + 1);
+            }
+        }
+     
+    
     
   }
 }
